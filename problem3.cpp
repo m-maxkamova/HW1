a07e21b9435099bacfeee1b4385329aedd1925eb
m-maxkamova <m.maxkamova@newuu.uz>
2024-09-15 20:59:16 +0500

    Delete problem3.cpp

--- problem3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int problem3() {
-    float tempInCelsius;
-    cin >> tempInCelsius;
-    float tempInFarenheit = tempInCelsius * (9./ 5) + 32;
-    cout << "The temperature in Farenheit is equal to " << tempInFarenheit;
-    return 0;
-}
